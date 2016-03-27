f39d085f05547971cce6a65672d105424cb2e3ff
alejes <alejes@yandex.ru>
2016-03-28 00:00:29 +0300

    energy + autosize fix

--- VADPhorms/Form1.Designer.cs
+++ VADPhorms/Form1.Designer.cs
@@ -24,13 +24,15 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.checkBoxWave = new System.Windows.Forms.CheckBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.checkBoxConstant = new System.Windows.Forms.CheckBox();
+            this.errorLog = new System.Windows.Forms.Label();
+            this.checkBoxEnergy = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -81,13 +83,14 @@
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
             this.chart1.Location = new System.Drawing.Point(-1, 61);
             this.chart1.Name = "chart1";
             this.chart1.Size = new System.Drawing.Size(714, 251);
             this.chart1.TabIndex = 4;
             this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // checkBoxConstant
             // 
@@ -100,11 +103,32 @@
             this.checkBoxConstant.UseVisualStyleBackColor = true;
             this.checkBoxConstant.CheckedChanged += new System.EventHandler(this.checkBoxConstant_CheckedChanged);
             // 
+            // errorLog
+            // 
+            this.errorLog.AutoSize = true;
+            this.errorLog.Location = new System.Drawing.Point(590, 358);
+            this.errorLog.Name = "errorLog";
+            this.errorLog.Size = new System.Drawing.Size(35, 13);
+            this.errorLog.TabIndex = 6;
+            this.errorLog.Text = "label1";
+            // 
+            // checkBoxEnergy
+            // 
+            this.checkBoxEnergy.AutoSize = true;
+            this.checkBoxEnergy.Location = new System.Drawing.Point(161, 358);
+            this.checkBoxEnergy.Name = "checkBoxEnergy";
+            this.checkBoxEnergy.Size = new System.Drawing.Size(59, 17);
+            this.checkBoxEnergy.TabIndex = 7;
+            this.checkBoxEnergy.Text = "Energy";
+            this.checkBoxEnergy.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(716, 380);
+            this.Controls.Add(this.checkBoxEnergy);
+            this.Controls.Add(this.errorLog);
             this.Controls.Add(this.checkBoxConstant);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.checkBoxWave);
@@ -128,6 +152,8 @@
         private System.Windows.Forms.CheckBox checkBoxWave;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.CheckBox checkBoxConstant;
+        private System.Windows.Forms.Label errorLog;
+        private System.Windows.Forms.CheckBox checkBoxEnergy;
     }
 }
 
--- VADPhorms/Form1.cs
+++ VADPhorms/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
 using NAudio.Wave;
 using NAudio.FileFormats;
 using NAudio.CoreAudioApi;
@@ -77,9 +78,15 @@ namespace VADPhorms {
         {
             if (checkBoxWave.Checked)
             {
+                errorLog.Text = e.BytesRecorded.ToString() + " " + e.GetHashCode();
                 for (var index = 0; index < e.BytesRecorded; index += 2)
                 {
                     double tmp = (short) ((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
+                    tmp /= 32768.0;
+                    //if (tmp > )
+                    //{
+
+                    //}
                     chart1.Series["wave"].Points.Add(tmp);
                     while (chart1.Series["wave"].Points.Count > 10000)
                     {
@@ -96,12 +103,44 @@ namespace VADPhorms {
             }
         }
 
+        private void ProcessEnergy(WaveInEventArgs e)
+        {
+            if (checkBoxEnergy.Checked)
+            {
+                double sum = 0;
+                for (var index = 0; index < e.BytesRecorded; index += 2)
+                {
+                    double tmp = (short)((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
+                    tmp /= 32768.0;
+                    sum += tmp*tmp;
+
+                }
+                for (var index = 0; index < e.BytesRecorded; index += 2)
+                {
+                    chart1.Series["energy"].Points.Add(sum);
+                    while (chart1.Series["energy"].Points.Count > 10000)
+                    {
+                        chart1.Series["energy"].Points.Remove(chart1.Series["energy"].Points.First());
+                    }
+                }
+            }
+            else
+            {
+                for (var index = 0; index < e.BytesRecorded; index += 2)
+                {
+                    if (chart1.Series["energy"].Points.Count <= 0) break;
+                    chart1.Series["energy"].Points.Remove(chart1.Series["energy"].Points.First());
+                }
+            }
+        }
+
         private void ProcessConstant(WaveInEventArgs e) {
             if (checkBoxConstant.Checked)
             {
                 for (var index = 0; index < e.BytesRecorded; index += 2)
                 {
                     //double tmp = (short)((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
+                    //tmp /= 32768.0;
                     chart1.Series["constant"].Points.Add(700);
                     while (chart1.Series["constant"].Points.Count > 10000)
                     {
@@ -126,6 +165,8 @@ namespace VADPhorms {
             // Порог для вычисления наличия речи
             ProcessWave(e);
             ProcessConstant(e);
+            ProcessEnergy(e);
+            chartArea.RecalculateAxesScale();
             bool result = false;
             bool Tr = false;
             double Sum2 = 0;
@@ -142,8 +183,34 @@ namespace VADPhorms {
             else { result = false; }
             return result;
         }
+
+        private ChartArea chartArea;
         public Form1() {
             InitializeComponent();
+
+            chartArea = chart1.ChartAreas.FindByName("ChartArea1") ?? new ChartArea();
+            chartArea.Name = "MyChartArea";
+            chartArea.AxisX.Title = "X";
+            chartArea.AxisY.Title = "Y";
+            //chartArea.AxisX.IntervalType = DateTimeIntervalType.Seconds;
+            //chartArea.AxisY2.MaximumAutoSize = 2.0F;
+            //chartArea.AxisX.Maximum = 1000;
+
+            //Начало записи
+            chart1.Series.Add("wave");
+            chart1.Series["wave"].ChartType =
+                System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            //chart1.Series["wave"].ChartArea = "ChartArea1";
+            chart1.Series.Add("constant");
+            chart1.Series["constant"].ChartType =
+                System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            chart1.Series.Add("energy");
+            chart1.Series["energy"].ChartType =
+                System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            //chart1.Series["constant"].ChartArea = "ChartArea1";
+            //chart1.ChartAreas.Add(chartArea);
+            //chart1.Series["constant"].Color = 
+
         }
 
         private void button1_Click(object sender, EventArgs e) {
@@ -161,17 +228,6 @@ namespace VADPhorms {
                 waveIn.WaveFormat = new WaveFormat(8000, 1);
                 //Инициализируем объект WaveFileWriter
                 writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
-                //Начало записи
-                chart1.Series.Add("wave");
-                chart1.Series["wave"].ChartType =
-                    System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
-                chart1.Series["wave"].ChartArea = "ChartArea1";
-                chart1.Series.Add("constant");
-                chart1.Series["constant"].ChartType =
-                    System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
-                chart1.Series["constant"].ChartArea = "ChartArea1";
-                //chart1.Series["constant"].Color = 
-
                 waveIn.StartRecording();
             }
             catch (Exception ex) {
@@ -197,6 +253,11 @@ namespace VADPhorms {
             //if (!checkBoxConstant.Checked){   
                 //chart1.Series["constant"].Points.Clear();
             //}
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

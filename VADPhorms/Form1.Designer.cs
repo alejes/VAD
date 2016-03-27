62885038a2a100233d9d43bc04d80ec902debb24
alejes <alejes@yandex.ru>
2016-03-27 22:08:32 +0300

    basic + original wave + constant wave

---
+++ VADPhorms/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace VADPhorms {
+    partial class Form1 {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBoxWave = new System.Windows.Forms.CheckBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBoxConstant = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 55);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // checkBoxWave
+            // 
+            this.checkBoxWave.AutoSize = true;
+            this.checkBoxWave.Checked = true;
+            this.checkBoxWave.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxWave.Location = new System.Drawing.Point(24, 358);
+            this.checkBoxWave.Name = "checkBoxWave";
+            this.checkBoxWave.Size = new System.Drawing.Size(55, 17);
+            this.checkBoxWave.TabIndex = 3;
+            this.checkBoxWave.Text = "Wave";
+            this.checkBoxWave.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(-1, 61);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(714, 251);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // checkBoxConstant
+            // 
+            this.checkBoxConstant.AutoSize = true;
+            this.checkBoxConstant.Location = new System.Drawing.Point(86, 358);
+            this.checkBoxConstant.Name = "checkBoxConstant";
+            this.checkBoxConstant.Size = new System.Drawing.Size(68, 17);
+            this.checkBoxConstant.TabIndex = 5;
+            this.checkBoxConstant.Text = "Constant";
+            this.checkBoxConstant.UseVisualStyleBackColor = true;
+            this.checkBoxConstant.CheckedChanged += new System.EventHandler(this.checkBoxConstant_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 380);
+            this.Controls.Add(this.checkBoxConstant);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.checkBoxWave);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBoxWave;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox checkBoxConstant;
+    }
+}
+
---
+++ VADPhorms/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NAudio.Wave;
+using NAudio.FileFormats;
+using NAudio.CoreAudioApi;
+using NAudio;
+
+namespace VADPhorms {
+    public partial class Form1 : Form {
+        // WaveIn - поток для записи
+        WaveIn waveIn;
+        //Класс для записи в файл
+        WaveFileWriter writer;
+        //Имя файла для записи
+        string outputFilename = "out.wav";
+
+        //Получение данных из входного буфера 
+        /*
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e) {
+            if (this.InvokeRequired) {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+            }
+            else {
+                //Записываем данные из буфера в файл
+                writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+            }
+        }
+        */
+        //получение данных из входного буфера для распознавания
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e) {
+            if (this.InvokeRequired) {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+            }
+            else {
+                //переменная для обозначения начала распознавания речевого отрезка
+                bool result = ProcessData(e);
+                // Если записываемый отрезок содержит речь
+                if (result == true) {
+                    // обрабатываем звуковые данные
+                    pictureBox1.Visible = true;
+                    writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+                }
+                else {
+                    pictureBox1.Visible = false;
+                    //MessageBox.Show("Речи небыло");
+                    // если речь не определена на звуковом отрезке
+                }
+            }
+        }
+        //Завершаем запись
+        void StopRecording() {
+            MessageBox.Show("StopRecording");
+            waveIn.StopRecording();
+        }
+        //Окончание записи
+        private void waveIn_RecordingStopped(object sender, EventArgs e) {
+            if (this.InvokeRequired) {
+                this.BeginInvoke(new EventHandler(waveIn_RecordingStopped), sender, e);
+            }
+            else {
+                waveIn.Dispose();
+                waveIn = null;
+                writer.Close();
+                writer = null;
+            }
+        }
+
+        private void ProcessWave(WaveInEventArgs e)
+        {
+            if (checkBoxWave.Checked)
+            {
+                for (var index = 0; index < e.BytesRecorded; index += 2)
+                {
+                    double tmp = (short) ((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
+                    chart1.Series["wave"].Points.Add(tmp);
+                    while (chart1.Series["wave"].Points.Count > 10000)
+                    {
+                        chart1.Series["wave"].Points.Remove(chart1.Series["wave"].Points.First());
+                    }
+                }
+            }
+            else
+            {
+                for (var index = 0; index < e.BytesRecorded; index += 2) {
+                    if (chart1.Series["wave"].Points.Count <= 0) break;
+                    chart1.Series["wave"].Points.Remove(chart1.Series["wave"].Points.First());
+                }
+            }
+        }
+
+        private void ProcessConstant(WaveInEventArgs e) {
+            if (checkBoxConstant.Checked)
+            {
+                for (var index = 0; index < e.BytesRecorded; index += 2)
+                {
+                    //double tmp = (short)((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
+                    chart1.Series["constant"].Points.Add(700);
+                    while (chart1.Series["constant"].Points.Count > 10000)
+                    {
+                        chart1.Series["constant"].Points.Remove(chart1.Series["constant"].Points.First());
+                    }
+                }
+            }
+            else
+            {
+                for (var index = 0; index < e.BytesRecorded; index += 2)
+                {
+                    if (chart1.Series["constant"].Points.Count <= 0) break;
+                        chart1.Series["constant"].Points.Remove(chart1.Series["constant"].Points.First());
+                }
+            }
+
+        }
+
+        private static double porog = 0.02;
+        // Обработчка речи - вычисляем, есть ли сама речь на звуковом отрезке
+        private bool ProcessData(WaveInEventArgs e) {
+            // Порог для вычисления наличия речи
+            ProcessWave(e);
+            ProcessConstant(e);
+            bool result = false;
+            bool Tr = false;
+            double Sum2 = 0;
+            int Count = e.BytesRecorded / 2;
+            for (int index = 0; index < e.BytesRecorded; index += 2) {
+                double Tmp = (short)((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
+                Tmp /= 32768.0;
+                Sum2 += Tmp * Tmp;
+                if (Tmp > porog)
+                    Tr = true;
+            }
+            Sum2 /= Count;
+            if (Tr || Sum2 > porog) { result = true; }
+            else { result = false; }
+            return result;
+        }
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+                MessageBox.Show("Start Recording");
+                waveIn = new WaveIn();
+                //Дефолтное устройство для записи (если оно имеется)
+                //встроенный микрофон ноутбука имеет номер 0
+                waveIn.DeviceNumber = 0;
+                //Прикрепляем к событию DataAvailable обработчик, возникающий при наличии записываемых данных
+                waveIn.DataAvailable += waveIn_DataAvailable;
+                //Прикрепляем обработчик завершения записи
+                waveIn.RecordingStopped += new EventHandler<StoppedEventArgs>(waveIn_RecordingStopped);
+                //Формат wav-файла - принимает параметры - частоту дискретизации и количество каналов(здесь mono)
+                waveIn.WaveFormat = new WaveFormat(8000, 1);
+                //Инициализируем объект WaveFileWriter
+                writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+                //Начало записи
+                chart1.Series.Add("wave");
+                chart1.Series["wave"].ChartType =
+                    System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+                chart1.Series["wave"].ChartArea = "ChartArea1";
+                chart1.Series.Add("constant");
+                chart1.Series["constant"].ChartType =
+                    System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+                chart1.Series["constant"].ChartArea = "ChartArea1";
+                //chart1.Series["constant"].Color = 
+
+                waveIn.StartRecording();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (waveIn != null) {
+                StopRecording();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e) {
+
+        }
+
+        private void checkBoxConstant_CheckedChanged(object sender, EventArgs e) {
+            //if (!checkBoxConstant.Checked){   
+                //chart1.Series["constant"].Points.Clear();
+            //}
+        }
+    }
+}
+
+
+
+
+
+

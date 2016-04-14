89f161760c2d64e36f11e4bbfe5d3ed28808a8aa
alejes <alejes@yandex.ru>
2016-04-14 17:05:00 +0300

    zcr realization

--- zcrIndicator.py
+++ zcrIndicator.py
@@ -1,19 +1,27 @@
 from indicators import *
 from register import *
+import numpy
 
 
 class zcrIndicator(Indicator):
+    fixedBounds = False
+
     @staticmethod
     def init():
         pass
 
     @staticmethod
     def data_process(data):
-        return data * 300
+        cnt = 0
+        for i in range(1, data.size - 1):
+            if data[i - 1] * data[i] < 0:
+                cnt += 1
 
-    pass
+        data.fill(cnt)
+
+        return data
 
 
 class waveIndicator_details:
-    print("wave indicator loaded")
+    print("zcr indicator loaded")
     Register.totalIndicators[Indicators.ZCR] = zcrIndicator

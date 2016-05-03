3d76df0c6793ddf6101d220e4406cf4ad94d850b
alejes <alejes@yandex.ru>
2016-05-03 22:48:35 +0300

    energy indicator norming start:

--- indicators/energyIndicator.py
+++ indicators/energyIndicator.py
@@ -1,12 +1,14 @@
 from indicators import *
 from register import *
+from collections import deque
 import math
 
 
 class energyIndicator(Indicator):
     fixedBounds = True
     boundMin = 0
-    boundMax = 50
+    boundMax = 1
+    maxEnergy = deque(maxlen=25)
 
     @staticmethod
     def init():
@@ -17,7 +19,17 @@ class energyIndicator(Indicator):
         sum = 0
         for dataFrame in data:
             sum += dataFrame * dataFrame
-        data.fill(math.sqrt(sum))
+
+        energyIndicator.maxEnergy.append(math.sqrt(sum))
+
+        currentMaxEnergy = max(1, max(energyIndicator.maxEnergy))
+
+        print(str(math.sqrt(sum) / currentMaxEnergy) + "  " + str(currentMaxEnergy))
+        if currentMaxEnergy > 0:
+            data.fill(math.sqrt(sum) / currentMaxEnergy)
+        else:
+            data.fill(math.sqrt(sum))
+
         return data
 
     pass
